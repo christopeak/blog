fed81b4601b51f30c348fbcfc57d608962c103d1
Chris Peak <christopeak@gmail.com>
2016-06-05 08:29:03 -0700

    add a few more view and stylesheet edits in order to push to github.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,25 @@
 class CommentsController < ApplicationController
   def add
+		@comment = Comment.new(params[:post_id])
   end
 
   def create
+		@comment = Comment.new(comment_params)
+		@comment.save
+		redirect_to posts_path
   end
 
   def delete
   end
+
+	def new
+		@comment = Comment.new
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:post_id, :body)
+	end
+
 end
